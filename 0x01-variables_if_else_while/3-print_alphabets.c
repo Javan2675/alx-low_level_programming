bff9d847774805f7a1af99b07c8cd1a1d9026e8f
javan2675 <javanmuriungi@gmail.com>
2022-07-01 21:09:08 +0000

    Lower and upper Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Variables and If
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
