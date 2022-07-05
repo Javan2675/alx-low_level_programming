b041ebc9fa27bde16e7683ca5d2af5e34a9ceff9
javan2675 <javanmuriungi@gmail.com>
2022-07-05 21:43:03 +0000

    Printing lower case letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing lower case letters
+ *
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
