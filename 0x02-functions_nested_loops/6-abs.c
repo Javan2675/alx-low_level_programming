7bf70dc269fce37511c85b4d5529b54fcdee48cc
javan2675 <javanmuriungi@gmail.com>
2022-07-06 13:46:04 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @n: the interger
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
