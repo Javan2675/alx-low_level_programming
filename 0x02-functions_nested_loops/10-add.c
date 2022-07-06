9c71b29731cb1fcc9027e60790a1a71a60ea468f
javan2675 <javanmuriungi@gmail.com>
2022-07-06 14:56:31 +0000

    sum of int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers and gives out result
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of the two intergers x and y
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
