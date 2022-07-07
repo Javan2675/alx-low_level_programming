480bbd0fd9e6b4befd81b8e8eaff02e0f74479cd
javan2675 <javanmuriungi@gmail.com>
2022-07-07 17:23:24 +0000

    Upper

---
+++ 0x04-more_functions_nested/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case characters
+ * @c: a character
+ *
+ * Return: 1 and 0 depending on the case
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
