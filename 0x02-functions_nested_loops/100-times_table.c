0e29f8617692422ecbb6719348bf0578392ad9d1
javan2675 <javanmuriungi@gmail.com>
2022-07-07 00:24:45 +0000

    Times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,37 +14,39 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; y++)
+		for (x = 0; x <= n; x++)
 		{
-			z = x * y;
-			if (y == 0)
+			for (y = 0; y <= n; y++)
 			{
-				_putchar(z + '0'); }
-			else if (z < 10 && y != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
+				z = x * y;
+				if (y == 0)
+				{
+					_putchar(z + '0'); }
+				else if (z < 10 && y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+				else if (z >= 10 && z < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
 			}
-			else if (z >= 10 && z < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((z / 10) + '0');
-				_putchar((z % 10) + '0');
-			}
-			else if (z >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((z / 100) + '0');
-				_putchar(((z / 10) % 10) + '0');
-				_putchar((z % 10) + '0');
-			}
+		_putchar('\n');
 		}
-	_putchar('\n');
 	}
-}
