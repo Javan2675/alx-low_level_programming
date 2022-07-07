f5caa512ca17ca3063bb3ac0b0a3bdd9032511ff
javan2675 <javanmuriungi@gmail.com>
2022-07-07 22:46:50 +0000

    product

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,10 @@
  * mul - multiplies two integers
  * @a: int to be multiplied to y
  * @b: int to be multiplied to x
+ *
  * Return: Product of the integers
  */
 int mul(int a, int b);
 {
-
 	return (a * b);
 }
