7e35f4880db8449f28e693a21760e2c2c9b291af
javan2675 <javanmuriungi@gmail.com>
2022-07-12 07:26:56 +0000

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string
+ *
+ * @s: string pointer
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int x, y;
+	char tmp;
+
+	x = 0;
+	y = 0;
+
+	while (s[i] != '\0')
+	{
+		x++;
+	}
+	x--;
+
+	for (y = 0; y <= x; y++)
+	{
+		tmp = s[y];
+		s[y] = s[x];
+		s[x] = tmp;
+		x--;
+	}
+}
