ae40726281cb5861b115d6153caa840b82a4fb5d
javan2675 <javanmuriungi@gmail.com>
2022-07-12 06:52:24 +0000

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: pointer of a string
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
