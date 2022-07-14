1d338efeca6ade3391826e4be78f3131aaaf4f7c
javan2675 <javanmuriungi@gmail.com>
2022-07-14 14:48:43 +0000

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,13 +15,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int j;
 
 
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[j] = src[j];
 
 	}
 	dest[j] = '\0';
