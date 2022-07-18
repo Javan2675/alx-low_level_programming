a4b712ef44c4ee86ba8676b37e964c5e6e9d4685
javan2675 <javanmuriungi@gmail.com>
2022-07-18 21:50:45 +0000

    Ptr

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - function that copies the n bytes from memory area
+ *
+ * @s: string pointer
+ *
+ * @to: string pointer
+ *
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
