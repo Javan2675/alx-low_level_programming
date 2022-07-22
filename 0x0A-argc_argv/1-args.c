aba09f158ebd4ddb1df24ce147f05518bad79c71
javan2675 <javanmuriungi@gmail.com>
2022-07-22 22:13:08 +0000

    Arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - function
+ *
+ * @argc: argumenent count
+ *
+ * @argv: arrays of passed arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
