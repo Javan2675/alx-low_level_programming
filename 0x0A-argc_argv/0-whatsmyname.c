ccb623248734129fbd667b00cef2cf4bfff360ab
javan2675 <javanmuriungi@gmail.com>
2022-07-22 22:08:12 +0000

    Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - function
+ *
+ * @argc: argumenent count
+ *
+ * @argv: arrays of passed arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
