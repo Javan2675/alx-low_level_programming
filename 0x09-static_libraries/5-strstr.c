3da97fafd7f769d742da417667bdf3f997717f60
javan2675 <javanmuriungi@gmail.com>
2022-07-22 21:31:15 +0000

    Static libraries

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strstr - copy two strings
+ *
+ * @src: String
+ *
+ * @dest: point of string
+ *
+ * @n: an integer
+ *
+ * Return: Dest
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+}
