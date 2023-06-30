47db04b832a5478a50186d2f18d663a256d5fc7c
Javan2675 <javanmuriungi@gmail.com>
2023-06-30 13:01:13 -0700

    working with strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: String pointer
+ *
+ * @s2: String pointer
+ *
+ * Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
